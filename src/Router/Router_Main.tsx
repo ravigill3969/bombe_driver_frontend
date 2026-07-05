506a738f663904e12b0e7a87bef7eebec9021cad
ravigill3936 <gill1234ravi@gmail.com>
2026-07-05 03:33:12 -0400

    your commit message here

--- src/Router/Router_Main.tsx
+++ src/Router/Router_Main.tsx
@@ -1,38 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "@/pages/Home";
 import LogIn from "@/pages/Login";
 import Register from "@/pages/Register";
 
-import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: Home,
-    children: [
-      { index: true, Component: Home },
-      {
-        path: "auth",
-        Component: Water,
-        children: [
-          { path: "login", Component: LogIn },
-          { path: "register", Component: Register },
-        ],
-      },
-    ],
+    element: <Home />,
+  },
+  {
+    path: "/auth/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/auth/register",
+    element: <Register />,
   },
 ]);
 
 function RouterMain() {
-  return (
-    <>
-      <RouterProvider router={router} />,
-    </>
-  );
-}
-
-function Water() {
-  return <div> water</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default RouterMain;
